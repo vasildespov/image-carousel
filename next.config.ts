5c9fdc9f7be211c4a1b5a08ddbb6693dda355210
Vasil Despov <60199602+vasildespov@users.noreply.github.com>
2025-10-28 09:22:50 +0200

    chore: setup next config to fetch images

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "dummyjson.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.dummyjson.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
