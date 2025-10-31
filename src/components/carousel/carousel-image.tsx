bdd58b33eeb95a42e41ab917bb153e32b8b49a43
Vasil Despov <60199602+vasildespov@users.noreply.github.com>
2025-11-01 01:10:42 +0200

    feat: figured out a way to add skeleton to image

--- src/components/carousel/carousel-image.tsx
+++ src/components/carousel/carousel-image.tsx
@@ -1,19 +1,45 @@
 import { cn } from "@/lib/utils";
 import NextImage from "next/image";
-import { ComponentProps } from "react";
+import { ComponentProps, useState } from "react";
+
+// Keeping the loaded images in "cache" allows for images to have loading state effect.
+// Otherwise, because there are unmount/remount events while looping the carousel,
+// the initial state gets reset and that causes a flicker which doesn't look good.
+const IMAGE_URL_CACHE = new Set<string>();
 
 export const CarouselImage = ({
-  alt,
   className,
   ...props
 }: ComponentProps<typeof NextImage>) => {
+  // handle case where props.src is static import
+  const src =
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    typeof props.src === "string" ? props.src : (props.src as any)?.src || "";
+
+  const [isLoading, setIsLoading] = useState(!IMAGE_URL_CACHE.has(src));
+
+  const handleOnLoad = () => {
+    IMAGE_URL_CACHE.add(src);
+    setIsLoading(false);
+  };
+
   return (
-    <NextImage
-      className={cn("object-cover", className)}
-      alt={alt}
-      fill
-      sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 25vw"
-      {...props}
-    />
+    <div className="relative w-full h-full">
+      {isLoading && (
+        <div className="absolute inset-0 bg-foreground/50 animate-pulse" />
+      )}
+
+      <NextImage
+        className={cn(
+          "object-cover transition-opacity",
+          isLoading ? "opacity-0" : "opacity-100",
+          className,
+        )}
+        onLoad={handleOnLoad}
+        fill
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 25vw"
+        {...props}
+      />
+    </div>
   );
 };
