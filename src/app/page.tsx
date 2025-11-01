6dc9dd1befa7b251e62e004c8fa05c074908d43f
Vasil Despov <60199602+vasildespov@users.noreply.github.com>
2025-11-01 22:24:23 +0200

    chore: add examples to root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,40 @@
-import { Carousel } from "@/components/carousel/carousel";
+import { ImageCarousel } from "@/components/image-carousel/image-carousel";
+import { VirtualisedList } from "@/components/virtualised-list";
 import { getPhotos } from "@/services/getPhotos";
 
 export default async function Home() {
   const photos = await getPhotos();
 
   return (
-    <div className="flex min-h-screen w-full items-center">
-      {photos.success && <Carousel data={photos.data} />}
+    <div className="flex gap-25 flex-col h-fit p-25">
+      <div>
+        <h1 className="mb-5 text-3xl">Image carousel</h1>
+        {photos.success && <ImageCarousel itemSize={200} data={photos.data} />}
+      </div>
+
+      <div>
+        <h1 className="mb-5 text-3xl">Image carousel with loop</h1>
+        {photos.success && (
+          <ImageCarousel gap={10} loop itemSize={200} data={photos.data} />
+        )}
+      </div>
+
+      <div>
+        <h1 className="mb-5 text-3xl">Vertical Image carousel</h1>
+        {photos.success && (
+          <ImageCarousel
+            className="max-h-100"
+            orientation="vertical"
+            itemSize={300}
+            data={photos.data}
+          />
+        )}
+      </div>
+
+      <div>
+        <h1 className="mb-5 text-3xl">Custom virtualized list</h1>
+        <VirtualisedList />
+      </div>
     </div>
   );
 }
