7c75e105a5659bb156e4bc7daac557a56bef59ff
Vasil Despov <60199602+vasildespov@users.noreply.github.com>
2025-11-01 22:23:39 +0200

    chore: add example implementation of hooks

---
+++ src/components/virtualised-list.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useVirtualisation } from "@/hooks/useVirtualisation";
+import { useMemo, useRef } from "react";
+
+export const VirtualisedList = () => {
+  const ref = useRef<HTMLDivElement>(null);
+  const data = useMemo(() => Array.from(Array(99999).keys()), []);
+
+  const { visibleData, wrapperStyle, containerStyle } = useVirtualisation({
+    data,
+    itemSize: 60,
+    orientation: "vertical",
+    ref: ref,
+  });
+
+  return (
+    <div ref={ref} className="overflow-auto border size-75">
+      <div style={containerStyle}>
+        <ul style={wrapperStyle}>
+          {visibleData.map(({ data, index }) => (
+            <li
+              key={index}
+              className="size-[60px] items-center justify-center flex border w-full"
+            >
+              {data}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
